315c54448135693043519cc994a00c4b63e971ca
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-09-09 04:55:49 -0400

    trying

---
+++ z/MAGA.R
@@ -0,0 +1,10 @@
+# Trial Run on Election Data#
+library(tidyverse)
+
+senate = read_csv('/Users/gzs/Desktop/MATH 503/2020election/z/senate_state.csv')
+house = read_csv('/Users/gzs/Desktop/MATH 503/2020election/z/house_district.csv')
+president = read_csv('/Users/gzs/Desktop/MATH 503/2020election/z/president_state.csv')
+
+
+# "Swing" Analysis #
+
