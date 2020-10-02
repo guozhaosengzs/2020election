d6c515598619705ed2096f57b7cc5a10e8b3a23d
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-02 01:55:27 -0400

    R Analysis

--- Zhaosen/all_states.R
+++ Zhaosen/all_states.R
@@ -7,4 +7,13 @@ data$State <- as.factor(data$State)
 train_df <- data %>% subset(Class == 'T') 
 pred_df <- data %>%  subset(Class == 'P')
 
-model1 = lm(data = train_df, President ~ )
+model1 = lm(data = train_df, President ~ House)
+summary(model1)
+layout(matrix(c(1,3,2,4),2,2))
+plot(model1)
+
+model2 = lm(data = train_df, President ~ House + State)
+summary(model2)
+plot(model2)
+
+###how do you fit and output again?
