fe458c6762180de832cf9929b6b2e5ebcd8b2e7c
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-04 14:43:18 -0400

    prediction results to electoral college votes

---
+++ Zhaosen/electoral_college.py
@@ -0,0 +1,127 @@
+import pandas as pd
+
+
+
+def main():
+    data = pd.read_csv('https://raw.githubusercontent.com/guozhaosengzs/2020election/master/Zhaosen/data/output.csv')
+
+    dict = us_states()
+    electoral = state_votes()
+
+
+
+def us_states():
+    statename_to_abbr = {
+        'Alabama': 'AL',
+        'Montana': 'MT',
+        'Alaska': 'AK',
+        'Nebraska': 'NE',
+        'Arizona': 'AZ',
+        'Nevada': 'NV',
+        'Arkansas': 'AR',
+        'New Hampshire': 'NH',
+        'California': 'CA',
+        'New Jersey': 'NJ',
+        'Colorado': 'CO',
+        'New Mexico': 'NM',
+        'Connecticut': 'CT',
+        'New York': 'NY',
+        'Delaware': 'DE',
+        'North Carolina': 'NC',
+        'Florida': 'FL',
+        'North Dakota': 'ND',
+        'Georgia': 'GA',
+        'Ohio': 'OH',
+        'Hawaii': 'HI',
+        'Oklahoma': 'OK',
+        'Idaho': 'ID',
+        'Oregon': 'OR',
+        'Illinois': 'IL',
+        'Pennsylvania': 'PA',
+        'Indiana': 'IN',
+        'Rhode Island': 'RI',
+        'Iowa': 'IA',
+        'South Carolina': 'SC',
+        'Kansas': 'KS',
+        'South Dakota': 'SD',
+        'Kentucky': 'KY',
+        'Tennessee': 'TN',
+        'Louisiana': 'LA',
+        'Texas': 'TX',
+        'Maine': 'ME',
+        'Utah': 'UT',
+        'Maryland': 'MD',
+        'Vermont': 'VT',
+        'Massachusetts': 'MA',
+        'Virginia': 'VA',
+        'Michigan': 'MI',
+        'Washington': 'WA',
+        'Minnesota': 'MN',
+        'West Virginia': 'WV',
+        'Mississippi': 'MS',
+        'Wisconsin': 'WI',
+        'Missouri': 'MO',
+        'Wyoming': 'WY'
+    }
+
+    return name_to_abbr
+
+def state_votes():
+    electoral_college = {
+        "Alabama":9,
+        "Alaska":3,
+        "Arizona":11,
+        "Arkansas":6,
+        "California":55,
+        "Colorado":9,
+        "Connecticut":7,
+        "District of Columbia":3,
+        "Delaware":3,
+        "Florida":29,
+        "Georgia":16,
+        "Hawaii":4,
+        "Idaho":4,
+        "Illinois":20,,
+        "Indiana":11,
+        "Iowa":6,
+        "Kansas":6,
+        "Kentucky":8,
+        "Louisiana":8,
+        "Maine":4,
+        "Maryland":10,
+        "Massachusetts":11,
+        "Michigan":16,
+        "Minnesota":10,
+        "Mississippi":6,
+        "Missouri":10,
+        "Montana":3,
+        "Nebraska":5,
+        "Nevada":6,
+        "New Hampshire":4,
+        "New Jersey":14,
+        "New Mexico":5,
+        "New York":29,
+        "North Carolina":15,
+        "North Dakota":3,
+        "Ohio":18,
+        "Oklahoma":7,
+        "Oregon":7,
+        "Pennsylvania":20,
+        "Rhode Island":4,
+        "South Carolina":9,
+        "South Dakota":3,
+        "Tennessee":11,
+        "Texas":38,
+        "Utah":6,
+        "Vermont":3,
+        "Virginia":13,
+        "Washington":12,
+        "West Virginia":5,
+        "Wisconsin":10,
+        "Wyoming":3
+    }
+
+    return electoral_college
+
+if __name__ == '__main__':
+    main()
