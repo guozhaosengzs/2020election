25f49d134f9f11f6e16b7c709440ab6862a2933f
mjankows <54722495+mjankows@users.noreply.github.com>
2020-10-20 09:06:42 -0400

    updated search data through 10-19

--- matt/electioncsvs2020.py
+++ matt/electioncsvs2020.py
@@ -10,20 +10,22 @@ from pytrends.request import TrendReq
 pytrend = TrendReq()
 
 listy = ['Trump', 'CNN', 
-           'Keep America Great', 'Benghazi',
+           'Keep America Great',
            'Fox News', 'Facebook', 'Instagram',
            'Twitter', 'fake news', 'black lives matter',
            'healthcare', 'immigration', 'racism',
            'Dow Jones', 'Russia', 'China', 'NASDAQ', 'Biden',
            'Sleepy Joe', 'Breonna Taylor', 'George Floyd', 'wildfires',
            'COVID-19', 'coronavirus', 'social distancing', 'face masks',
-           'white supremacy', 'Trump covid']
+           'white supremacy', 'Trump covid', 'Apple', 'presidential debate',
+           'stimulus package', 'Mike Pence', 'Alexandra Ocasio-Cortez',
+           'Kamala Harris']
 
 dfs = []
 
 for item in listy:
     pytrend.build_payload(kw_list=[item], geo='US',
-                          timeframe='2020-08-01 2020-10-3')
+                          timeframe='2020-08-01 2020-10-19')
 
     dfs.append(pytrend.interest_by_region())
     
